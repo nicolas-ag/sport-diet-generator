dfa4d7f85e78bf5093e001515bd13898ebb71301
nicolas-ag <60199181+nicolas-ag@users.noreply.github.com>
2023-07-25 15:13:31 -0400

    energy expenditure calculation and display modifications
    energy expenditure calculation and display modifications

--- sport-diet-generator/src/app/app.component.ts
+++ sport-diet-generator/src/app/app.component.ts
@@ -16,7 +16,9 @@ export class AppComponent implements OnInit {
   ageInput: number = 0;
   heightInput: number = 0;
   weightInput: number = 0;
-  nbCalories: number = 0;
+  levelInput: number = 0;
+  nbCaloriesBasal: number = 0;
+  nbCaloriesGoals: number = 0;
 
   title = 'sport-diet-generator';
   faPerson = faPerson;
@@ -24,19 +26,50 @@ export class AppComponent implements OnInit {
   faPersonRunning = faPersonRunning;
   faPersonSwimming = faPersonSwimming;
 
+  ratiosGaosls: any = {
+    None: {
+      value: 1.2,
+    },
+    Little: {
+      value: 1.375,
+    },
+    Medium: {
+      value: 1.55,
+    },
+    Hard: {
+      value: 1.725,
+    },
+    Expert: {
+      value: 1.9,
+    },
+  };
+
   ngOnInit(): void {}
 
   estimatedCaloriesDaily(): void {
     if (this.sexeInput == '1') {
-      this.nbCalories = (
-        (13.707*(this.weightInput)) + (this.heightInput*492.3) - (this.ageInput*6.673) 
-      ) + 77.607;
-    }
-    else if(this.sexeInput == '2') {
-      this.nbCalories = (
-        (9.740*(this.weightInput)) + (this.heightInput*172.9) - (this.ageInput*4.737) 
-      ) + 77.607;
+      this.nbCaloriesBasal = Math.trunc(
+        13.707 * this.weightInput +
+          this.heightInput * 492.3 -
+          this.ageInput * 6.673 +
+          77.607
+      );
+    } else if (this.sexeInput == '2') {
+      this.nbCaloriesBasal = Math.trunc(
+        9.74 * this.weightInput +
+          this.heightInput * 172.9 -
+          this.ageInput * 4.737 +
+          77.607
+      );
     }
   }
 
+  changeLevel(test: string): void {
+    this.levelInput = this.ratiosGaosls[test].value;
+    this.estimatedCaloriesWithGoals();
+  }
+
+  estimatedCaloriesWithGoals(): void {
+    this.nbCaloriesGoals = Math.trunc(this.nbCaloriesBasal * this.levelInput);
+  }
 }
